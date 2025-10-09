a7814ddb3446615764b0b425f71a2aa237b1a2e0
angelostori <angelostori13@gmail.com>
2025-10-09 16:22:57 +0200

    define an array with the values, create a random number between 1 and 6 and assign it to the array index for estract two random values

--- JS-DADI/script.js
+++ JS-DADI/script.js
@@ -7,3 +7,18 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 */
+//definisco un array con i valori della faccia del dado
+const facceDado = [1, 2, 3, 4, 5, 6]
+//creo due numeri casuali
+let numeroCasuale1 = parseInt((Math.random() * 6) + 1)
+let numeroCasuale2 = parseInt((Math.random() * 6) + 1)
+//uso i numeri casuali come indice per estrarre una faccia del dado
+const numeroUtente = facceDado.at(numeroCasuale1)
+const numeroComputer = facceDado.at(numeroCasuale2)
+//stampo dei messaggi con il risultato
+console.log(`Il risultato del tuo lancio è: ${numeroCasuale1}`)
+console.log(`Il risultato del lancio del computer è: ${numeroCasuale2}`);
+
+
+
+
